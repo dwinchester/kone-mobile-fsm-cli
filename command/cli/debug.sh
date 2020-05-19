e7cd382bab868a709c0254246b22cb4ac488bcdc
David Winchester <lwinchester@salesforce.com>
2020-05-19 15:39:21 +0200

    Added help option to debug command

--- command/cli/debug.sh
+++ command/cli/debug.sh
@@ -3,18 +3,27 @@
 set -e
 . "${ROOT_DIR}/utils.sh"
 
+help_arg=2
+if [ "${!help_arg}" == "help" ]; then
+  say "Usage: kone cli debug"  
+  exit 3
+fi
+
 # get the current profile value
 debug="${DEBUG}"
+message=""
 
 # toggle the debug flag
 if [ "${debug}" = true ]; then
   debug=false
+  message="Debug has been disabled"
 else 
   debug=true
+  message="Debug has been enabled"
 fi
 
 # set the debug value in configured profile
 "${ROOT_DIR}/command/configure/set.sh" --variable DEBUG --value "${debug}"
 
-say "${green:-}Success${normal:-} Debug enabled."
+say "${yellow:-}Warning${normal:-} ${message}."
 exit 0
