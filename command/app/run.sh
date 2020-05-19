9992501f91558746d9e60a7d7e293621465ebc4b
David Winchester <lwinchester@salesforce.com>
2020-05-19 19:52:34 +0200

    Added device option to run command

--- command/app/run.sh
+++ command/app/run.sh
@@ -3,7 +3,45 @@
 set -e
 . "${ROOT_DIR}/utils.sh"
 
+show_help() {
+  echo ""
+  echo "Description: Run a package on a device."   
+  echo "Usage: kone app run [options]"
+  echo ""
+  echo "Options:"
+  echo "  -h, --help                Show command line help."  
+  echo "  --device <DEVICE_ID>      The name of the device."
+  echo ""
+  echo "Examples:"
+  echo "  kone app run --help"
+  echo "  kone app run"
+  echo "  kone app run --device ZY32298W3J"
+  echo "" 
+}
+
 device="${DEVICE_ID}"
+
+while [ $# -ne 0 ]
+do
+  key="${1}" 
+  case "${key}" in
+    -h|--help|help)
+      show_help
+      exit 3
+      ;;
+    run)
+      ;;
+    --device)
+      shift
+      device="${1}"
+      ;;
+    *)
+      say_err "$(unknown_command_message "${key}")"
+      exit 3
+      ;;
+  esac
+  shift
+done
 
 cd "${HOME}/${ANDROID_HOME}/platform-tools"
 
