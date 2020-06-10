981521f318aa2a0eca5759c134755bce52eae78c
David Winchester <lwinchester@salesforce.com>
2020-06-10 10:55:49 +0200

    Moved dev commands to dev dir and updated help files

--- command/dev/list-tags.sh
+++ command/dev/list-tags.sh
@@ -9,7 +9,7 @@ cd "${project_path}"
 arg_start=1
 pattern="$((arg_start+1))"
 
-# list the all tags. Running the command `git tag` implicitly assumes you want 
+# list  all tags. Running the command `git tag` implicitly assumes you want 
 # a listing. If, however, a wildcard pattern is provided to match tag names, 
 # the use of -l or --list is mandatory
 git tag -l 
