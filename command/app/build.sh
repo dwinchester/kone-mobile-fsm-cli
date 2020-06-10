e5075c66c0b65f452a328e37c6f64c262d4d6518
David Winchester <lwinchester@salesforce.com>
2020-06-10 09:57:52 +0200

    Added restore command to remove and refresh gradle dependencies

--- command/app/build.sh
+++ command/app/build.sh
@@ -12,9 +12,10 @@ show_help() {
   echo "  -h, --help                      Show command line help." 
   echo "  --build-type <BUILD_TYPE>       The version of the app that you want to build."
   echo "  --no-build                      Doesn't build the project before running. It also implicitly sets the --no-restore flag."
-  echo "  --no-dependencies               Doesn't execute an implicit restore on the project dependencies when running the command."
+  echo "  --no-dependencies               Doesn't reinstall project dependencies when running the command."
   echo "  --output <OUTPUT_DIRECTORY>     Specifies the path for the output directory."
   echo "  --profile <PROFILE>             Specifies the named profile to use for this command." 
+  echo "  --refresh-dependencies          Execute an implicit gradle dependencies refresh when running the command."
   echo "  --stacktrace                    Show truncated stacktraces from the configured build task." 
   echo ""
   echo "Examples:"
@@ -53,6 +54,9 @@ do
       ;;
     --stacktrace)
       non_dynamic_params+=" --stacktrace"
+      ;;
+    --refresh-dependencies)
+      non_dynamic_params+=" --refresh-dependencies"
       ;;
     *)
       say_err "$(unknown_command_message "${key}")"
---
+++ command/app/restore.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -e
+. "${ROOT_DIR}/utils.sh"
+
+# delete all the existing artifacts and metadata Gradle has previously downloaded
+rm -rf "${HOME}"/.gradle/caches/
+
+gradlew_path="$(combine_paths $(get_project_path) "/android/FieldService-Android")"
+cd "${gradlew_path}"
+
+./gradlew clean --refresh-dependencies
+exit 0
