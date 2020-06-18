883e3d37aabda16f282b5da4bf0f1928a0e99438
David Winchester <lwinchester@salesforce.com>
2020-06-18 14:55:52 +0200

    Fixed CLI updat git commands

--- command/cli/update-cli.sh
+++ command/cli/update-cli.sh
@@ -6,18 +6,7 @@ set -e
 cd "${ROOT_DIR}"
 
 say "Checking for updates."
+git pull
 
-changed=0
-message=""
-
-git remote update && git status -uno | grep -q 'Your branch is behind.' && changed=1
-
-if [ {${changed}} = 1 ]; then
-  git pull
-  message="CLI update completed.";
-else
-  message="CLI up-to-date. No changes found."
-fi
-
-say "${green:-}Success${normal:-} ${message}"
+say "${green:-}Success${normal:-} CLI up-to-date."
 exit 0
