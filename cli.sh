442cd9b9d2720b674e47455009d497a35668650f
David Winchester <lwinchester@salesforce.com>
2020-10-09 13:59:31 +0200

    Refactored cli entrypoint for cleaner implementation, added new cli terminal logo and colors to welcome message

--- cli.sh
+++ cli.sh
@@ -10,93 +10,9 @@ set -e
 exec 3>&1
 
 export ROOT_DIR=$(cd $(dirname ${0}) && pwd)
-script_name=$(basename "${0}")
 
 # shellcheck external sources
-. "${ROOT_DIR}/sysfetch.sh"
 . "${ROOT_DIR}/utils.sh"
 
-invocation='say_verbose "Calling: ${FUNCNAME[0]} $*$"' # Use in functions: eval $invocation
-
-# export configs to ensure that all settings are visible to all commands
-CLI_CONFIG_FILE=$( combine_paths "${ROOT_DIR}" "/cli.settings" )
-export $(cat "${CLI_CONFIG_FILE}" | xargs)
-
-# check that the default profile exists 
-profile=$( combine_paths "${ROOT_DIR}" "${DEFAULT_PROFILE}" )
-if [[ ! -f "${profile}" ]]; then
-  say_err "No configruation profile found. Copy "`/template.settings`" to "`${profile}`" and adjust."
-  exit 1
-fi
-export $(cat "${profile}" | xargs)
-
-# if no arguments are passed, then show the welcome message
-if [ $# == 0 ]; then
-  echo ""  
-  echo "Welcome to the KONE Mobile FSM CLI"
-  echo "------------------------------------"
-  echo "CLI version: ${CLI_VERSION}"
-  echo "Current profile: ${DEFAULT_PROFILE}"
-  echo ""
-  echo "Use 'kone help' to see available options and commands or visit the project wiki."
-  echo "Report issues and find source on GitHub: https://github.com/dwinchester/kone-mobile-fsm-cli"
-  echo ""
-  exit 3
-fi
-
-# locate the correct command to execute by looking through the commands directory
-# for folders and files and match the arguments
-arg_start=1
-cmd_name="${!arg_start}"
-cmd_file="${ROOT_DIR}/command/${cmd_name}"
-
-while [[ -d "${cmd_file}" && $arg_start -le $# ]]; do
-  # if the user provides help as the last argument, then display help
-  if [[ "${!arg_start}" == "help" ]]; then
-    # strip off the "help" portion of the command
-    args=("$@")
-    unset "args[$((arg_start-1))]"
-    args=("${args[@]}")
-
-    "${ROOT_DIR}/help.sh" $0 ${args[@]}
-    exit 3
-  fi
-
-  arg_start=$(($arg_start+1))
-
-  if [ -z "${!arg_start}" ]; then
-    cmd_file="${cmd_file}/${cmd_name}.sh" 
-  else
-    cmd_file="${cmd_file}/${!arg_start}.sh" 
-  fi 
-done
-
-# place the arguments for the command in their own list to make them easier 
-# to work with
-cmd_args=("${@:arg_start}")
-
-# if we hit a directory by the time we run out of arguments, then our user
-# hasn't completed their command, so we'll show them the help for that directory
-if [ -d "${cmd_file}" ]; then
-  "${ROOT_DIR}/help.sh" $0 $@
-  exit 3
-fi
-
-# if a command could not be found, then display an error message
-if [[ ! -f "${cmd_file}" ]]; then
-  "${ROOT_DIR}/help.sh" $0 ${@:1:$(($arg_start-1))}
-  exit 3
-fi
-
-# run the command and capture its exit code for introspection
-"${cmd_file}" ${cmd_args[@]}
-EXIT_CODE=$?
-
-# if the command exited with an exit code of 3 then show the help 
-# documentation
-if [[ ${EXIT_CODE} == 3 ]]; then
-  say_warning 'Exited with code 3.'
-fi
-
-# exit with the same code as the command
-exit $EXIT_CODE
+# run the CLI entrypoint
+cli_entrypoint "$@"
--- utils.sh
+++ utils.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+. "${ROOT_DIR}/colors_formatting.sh"
+. "${ROOT_DIR}/sysfetch.sh"
+
+invocation='say_verbose "Calling: ${FUNCNAME[0]} $*$"' # Use in functions: eval $invocation
+
 # Setup some colors to use. These need to work in fairly limited shells.
 # See if stdout is a terminal
 if [ -t 1 ] && command -v tput > /dev/null; then
@@ -19,6 +24,30 @@ if [ -t 1 ] && command -v tput > /dev/null; then
   fi
 fi
 
+show_cli_welcome() {
+  echo "${COLOR_BLUE}"  
+  echo " /::   /::  /::::::  /::   /:: /::::::::        /::::::  /::       /::::::"
+  echo "| ::  /::/ /::__  ::| ::: | ::| ::_____/       /::__  ::| ::      |_  ::_/"
+  echo "| :: /::/ | ::  \ ::| ::::| ::| ::            | ::  \__/| ::        | ::  "
+  echo "| :::::/  | ::  | ::| :: :: ::| :::::         | ::      | ::        | ::  "
+  echo "| ::  ::  | ::  | ::| ::  ::::| ::__/         | ::      | ::        | ::  "
+  echo "| ::\  :: | ::  | ::| ::\  :::| ::            | ::    ::| ::        | ::  "
+  echo "| :: \  ::|  ::::::/| ::$ \  ::| ::::::::      |  ::::::/| :::::::: /::::::"
+  echo "|__/  \__/ \______/ |__/  \__/|________/       \______/ |________/|______/"
+  echo "${COLOR_NORMAL}"
+  echo ""
+  echo "Welcome to the KONE Mobile FSM CLI"
+  echo "------------------------------------"
+  echo "Version:  ${COLOR_YELLOW}${CLI_VERSION}${COLOR_NORMAL}"
+  echo ""
+  echo "${COLOR_CYAN}To see your CLI configuration run 'kone configure list'."
+  echo ""
+  echo "Use the command 'kone help' to see available options or visit the project wiki."
+  echo "Report issues and find source on GitHub: ${TEXT_UNDERLINE}https://github.com/dwinchester/kone-mobile-fsm-cli${TEXT_UNDERLINE_RESET}${COLOR_NORMAL}"
+  echo ""
+  exit 3
+}
+
 unknown_command_message() {
   local cmd="${1}"
 
@@ -28,6 +57,87 @@ unknown_command_message() {
   echo "  * You intended to execute the kone app, but the kone command ${cmd} does not exist."
   echo "  * You intended to run a global tool, but a kone-prefixed executable with this name could not be found on the PATH."
   echo "" 
+}
+
+cli_profile() {
+  # export configs to ensure that all settings are visible to all commands through the entrypoint
+  CLI_CONFIG_FILE=$( combine_paths "${ROOT_DIR}" "/cli.settings" )
+  export $(cat "${CLI_CONFIG_FILE}" | xargs)
+
+  # check that the default profile exists 
+  profile=$( combine_paths "${ROOT_DIR}" "${DEFAULT_PROFILE}" )
+  if [[ ! -f "${profile}" ]]; then
+    say_err "No configruation profile found. Copy "`/template.settings`" to "`${profile}`" and adjust."
+    exit 1
+  fi
+  export $(cat "${profile}" | xargs)
+  return 0
+}
+
+cli_entrypoint() {
+  cli_profile
+
+  # if no arguments are passed, then show the welcome message
+  if [ $# == 0 ]; then
+    show_cli_welcome
+  fi
+
+  # locate the correct command to execute by looking through the commands directory
+  # for folders and files and match the arguments
+  arg_start=1
+  cmd_name="${!arg_start}"
+  cmd_file="${ROOT_DIR}/command/${cmd_name}"
+
+  while [[ -d "${cmd_file}" && $arg_start -le $# ]]; do
+    # if the user provides help as the last argument, then display help
+    if [[ "${!arg_start}" == "help" ]]; then
+      # strip off the "help" portion of the command
+      args=("$@")
+      unset "args[$((arg_start-1))]"
+      args=("${args[@]}")
+
+      "${ROOT_DIR}/help.sh" $0 ${args[@]}
+      exit 3
+    fi
+
+    arg_start=$(($arg_start+1))
+
+    if [ -z "${!arg_start}" ]; then
+      cmd_file="${cmd_file}/${cmd_name}.sh" 
+    else
+      cmd_file="${cmd_file}/${!arg_start}.sh" 
+    fi 
+  done
+
+  # place the arguments for the command in their own list to make them easier 
+  # to work with
+  cmd_args=("${@:arg_start}")
+
+  # if we hit a directory by the time we run out of arguments, then our user
+  # hasn't completed their command, so we'll show them the help for that directory
+  if [ -d "${cmd_file}" ]; then
+    "${ROOT_DIR}/help.sh" $0 $@
+    exit 3
+  fi
+
+  # if a command could not be found, then display an error message
+  if [[ ! -f "${cmd_file}" ]]; then
+    "${ROOT_DIR}/help.sh" $0 ${@:1:$(($arg_start-1))}
+    exit 3
+  fi
+
+  # run the command and capture its exit code for introspection
+  "${cmd_file}" ${cmd_args[@]}
+  EXIT_CODE=$?
+
+  # if the command exited with an exit code of 3 then show the help 
+  # documentation
+  if [[ ${EXIT_CODE} == 3 ]]; then
+    say_warning 'Exited with code 3.'
+  fi
+
+  # exit with the same code as the command
+  exit $EXIT_CODE
 }
 
 say_warning() {
@@ -53,7 +163,7 @@ say_verbose() {
 debug() {
   if [ "${DEBUG}" = true ]; then
     local now="$(date +"%Y%m%d%I%M")"
-    local log="${ROOT_DIR}/log/android-debug-${now}.log" # output: release-202004240958.log
+    local log="${ROOT_DIR}/log/cli-debug-${now}.log" # output: cli-debug-202004240958.log
 
     printf 'Log File - ' > "${log}"
     
@@ -110,7 +220,7 @@ to_upper() {
 }
 
 remove_trailing_slash() {
-  #eval $invocation
+  eval $invocation
 
   local input="${1:-}"
   echo "${input%/}"
@@ -118,7 +228,7 @@ remove_trailing_slash() {
 }
 
 remove_beginning_slash() {
-  #eval $invocation
+  eval $invocation
 
   local input="${1:-}"
   echo "${input#/}"
@@ -201,7 +311,6 @@ set_config() {
   eval $invocation
 
   value_esc=$( echo $2 | sed 's_/_\\/_g' )
-
   sed -i '' "s/\($(to_upper $1) *= *\).*/\1$value_esc/" "${3}"
 
   return 0
