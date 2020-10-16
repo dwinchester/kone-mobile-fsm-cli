04eefff7fa26c32d95008f4cf0430ee7588759f2
David Winchester <lwinchester@salesforce.com>
2020-10-16 10:20:52 +0200

    Updated info and cli-update commands for color formatting

--- command/cli/info.sh
+++ command/cli/info.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 set -e
+. "${ROOT_DIR}/utils/utils.sh"
 . "${ROOT_DIR}/utils/sysfetch.sh"
 
 print_sysinfo() {
@@ -8,17 +9,17 @@ print_sysinfo() {
   cd "${ROOT_DIR}" && commit=$( git rev-parse --short HEAD );
 
   echo ""
-  echo "KONE Mobile FSM CLI:"
-  echo "  Version: ${CLI_VERSION}"
-  echo "  Commit: ${commit}"
+  echo "${COLOR_DARK_GRAY}KONE CLI:${COLOR_NORMAL}"
+  echo "Version: ${CLI_VERSION}"
+  echo "Commit: ${commit}"
   echo ""
-  echo "CLI settings:"
-  echo "  Project Path: $( get_project_path )"
-  echo "  Primary branch: ${BRANCH_MAIN}"
-  echo "  Submodule branch: ${BRANCH_SUBMODULE}"
-  echo "  Version: ${VERSION_TAG}"
-  echo "  Device ID: ${DEVICE_ID}"
-  echo "  Logging Files Path: ${LOGGING_FILES_PATH}"
+  echo "${COLOR_DARK_GRAY}SETTINGS:${COLOR_NORMAL}"
+  echo "Project Path: $( get_project_path )"
+  echo "Primary branch: ${BRANCH_MAIN}"
+  echo "Submodule branch: ${BRANCH_SUBMODULE}"
+  echo "Version Tag: ${VERSION_TAG}"
+  echo "Device ID: ${DEVICE_ID}"
+  echo "Logging Files Path: ${LOGGING_FILES_PATH}"
   echo ""
   exit 3
 }
--- command/cli/update.sh
+++ command/cli/update.sh
@@ -10,5 +10,5 @@ say "Checking for updates."
 # get all changes from the remote repository
 git pull >/dev/null
 
-say "${BACKGROUND_GREEN}Success${BACKGROUND_NORMAL} CLI up to date."
+say "${BACKGROUND_GREEN}Success${BACKGROUND_NORMAL} ${COLOR_GREEN}CLI up-to-date.${COLOR_NORMAL}"
 exit 0
