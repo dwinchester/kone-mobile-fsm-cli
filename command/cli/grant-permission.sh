b90c6cde4e932f07253610adc0a5fbfb412a4d00
David Winchester <lwinchester@salesforce.com>
2020-10-26 10:24:13 +0100

    Added success message to grant-permission command

--- command/cli/grant-permission.sh
+++ command/cli/grant-permission.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 chmod -R 775 "${HOME}/.kone/" # update execute permissions
+
+say "${BACKGROUND_GREEN}Success${BACKGROUND_NORMAL} ${COLOR_GREEN}Recursively set permissions on all CLI files to 775.${COLOR_NORMAL}"
