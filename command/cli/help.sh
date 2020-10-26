2dfe697f17b32f90503d26756a9dc9544c69fc16
David Winchester <lwinchester@salesforce.com>
2020-10-26 10:17:10 +0100

    Changed update command name to update-cli to fit with pattern

--- command/cli/help.sh
+++ command/cli/help.sh
@@ -17,7 +17,7 @@ echo "${COLOR_CYAN}debug${COLOR_NORMAL}               ${COLOR_LIGHT_GRAY}Enable 
 echo "${COLOR_CYAN}delete-command${COLOR_NORMAL}      ${COLOR_LIGHT_GRAY}Delete a CLI command directory.${COLOR_NORMAL}"
 echo "${COLOR_CYAN}grant-permission${COLOR_NORMAL}    ${COLOR_LIGHT_GRAY}Recursively makes all permissions 775 on the cli directory.${COLOR_NORMAL}"
 echo "${COLOR_CYAN}help${COLOR_NORMAL}                ${COLOR_LIGHT_GRAY}Show command line help.${COLOR_NORMAL}"
-echo "${COLOR_CYAN}update${COLOR_NORMAL}              ${COLOR_LIGHT_GRAY}Updates the CLI to the latest version.${COLOR_NORMAL}"
+echo "${COLOR_CYAN}update-cli${COLOR_NORMAL}          ${COLOR_LIGHT_GRAY}Updates the CLI to the latest version.${COLOR_NORMAL}"
 echo "${COLOR_CYAN}version${COLOR_NORMAL}             ${COLOR_LIGHT_GRAY}Prints out the version of the CLI in use.${COLOR_NORMAL}" 
 echo ""
 echo "Or, simply run:"
